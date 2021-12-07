ce9a3c8248b089755cba7c9edeb21bb0b9885de1
nayeemuddin-bd100 <nayeemuddin.bd100@gmail.com>
2021-12-08 01:31:30 +0600

    .

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,14 +1,30 @@
+import AddReactionIcon from '@mui/icons-material/AddReaction';
+import SendIcon from "@mui/icons-material/Send";
 import { Button, Container, Typography } from '@mui/material';
-import React from 'react';
 
 export default function Create() {
   return (
-    <Container >
+    <Container>
       <Typography variant="h6" color="textSecondary" gutterBottom>
-        Create New Page 
+        Create New Page
       </Typography>
 
-      <Button type="submit" variant='contained' color="primary" > Submit </Button>
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        startIcon={<AddReactionIcon color="success" />}
+        endIcon={<SendIcon/>}
+      >
+        {" "}
+        Submit{" "}
+      </Button>
+
+      {/* Icons
+      <br/>
+      <AddReactionIcon color='success'/>
+      <AddReactionIcon color='warning' fontSize='small' />
+      <AddReactionIcon /> */}
     </Container>
   );
 }
