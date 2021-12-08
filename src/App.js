bd5467886fd576a430e7215a486b64724e3b62bc
nayeemuddin-bd100 <nayeemuddin.bd100@gmail.com>
2021-12-08 16:22:33 +0600

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Notes from './pages/Notes'
-import Create from './pages/Create'
+import { createTheme, ThemeProvider } from '@mui/material';
+import { red } from '@mui/material/colors';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './pages/Create';
+import Notes from './pages/Notes';
+
+
+// override defult theme
+const theme = createTheme({
+  palette: {
+    // secondary: {
+    //   main: "#efefef",
+    // },
+    secondary: {
+      main: red[900], // colors are must've to imported from mui-colors
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,30 +1,64 @@
-import AddReactionIcon from '@mui/icons-material/AddReaction';
+import AddReactionIcon from "@mui/icons-material/AddReaction";
 import SendIcon from "@mui/icons-material/Send";
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+// const useStyles = makeStyles({}) // create custom style by using this hooks
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+    border: 0,
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 48,
+    padding: "0 30px",
+    "&:hover": {
+      background: "blue",
+    },
+  },
+  title: {
+    textDecoration: "underline",
+    marginBottom:20
+  }
+});
 
 export default function Create() {
+  const classes = useStyles();
   return (
     <Container>
-      <Typography variant="h6" color="textSecondary" gutterBottom>
+      <Typography
+        className={classes.title}
+        variant="h6"
+        color="textSecondary"
+        gutterBottom
+      >
         Create New Page
       </Typography>
-
       <Button
         type="submit"
         variant="contained"
         color="primary"
         startIcon={<AddReactionIcon color="success" />}
-        endIcon={<SendIcon/>}
+        endIcon={<SendIcon />}
       >
-        {" "}
-        Submit{" "}
+        Submit
       </Button>
-
+      <br />
+      <br />
+      <br />
       {/* Icons
       <br/>
-      <AddReactionIcon color='success'/>
-      <AddReactionIcon color='warning' fontSize='small' />
-      <AddReactionIcon /> */}
+      <AddReactionIcon color='success' fontSize='small | large' />
+      */}
+
+      {/* Custom hooks(makeStyles) provided by mui */}
+      <Button className={classes.root}>
+        {" "}
+        Custom styles with mui hooks api
+      </Button>
+
+      {/* Testing override default theme or styles */}
+      <Typography color="secondary"> changed secondary text color to red</Typography>
     </Container>
   );
 }
